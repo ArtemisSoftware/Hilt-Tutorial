17d05870d115a8c57fa92428a8e1c4a9e297c2bb
gustavo <gustavo.maia@b2b.com.pt>
2020-11-25 10:03:48 +0000

    Scoping with hilt

---
+++ app/src/main/java/com/artemissoftware/hilttutorial/MyFragment.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.hilttutorial
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class MyFragment : Fragment(){
+
+    @Inject
+    lateinit var someClass: SomeClass
+
+}
--- app/src/main/java/com/artemissoftware/hilttutorial/SomeClass.kt
+++ app/src/main/java/com/artemissoftware/hilttutorial/SomeClass.kt
@@ -1,7 +1,10 @@
 package com.artemissoftware.hilttutorial
 
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@ActivityScoped
 class SomeClass @Inject constructor(private val someOtherClass: SomeOtherClass){
 
     fun doAThing(): String{
