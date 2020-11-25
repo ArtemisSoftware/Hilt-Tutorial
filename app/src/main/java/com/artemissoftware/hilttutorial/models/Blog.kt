8e4824a2e7482898ac4be1877702628e308048f2
gustavo <gustavo.maia@b2b.com.pt>
2020-11-25 14:42:01 +0000

    Blog class

---
+++ app/src/main/java/com/artemissoftware/hilttutorial/models/Blog.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.hilttutorial.models
+
+data class Blog(var id: Int,
+                var title: String,
+                var body: String,
+                var image: String,
+                var category: String)
